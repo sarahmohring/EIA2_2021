037108db8af7f516eb23eb5e16b96b267d6b9a21
sarahmohring <sarah.erika.sigrid.mohring@hs-furtwangen.de>
2021-05-07 15:39:47 +0200

    verbesserung canvas

--- L04_Canvas/Sarah/generative_kunst.js
+++ L04_Canvas/Sarah/generative_kunst.js
@@ -1,6 +1,6 @@
 "use strict";
-var GenerativeKunst;
-(function (GenerativeKunst) {
+var L04_GenerativeKunst;
+(function (L04_GenerativeKunst) {
     window.addEventListener("load", fillCanvas);
     let canvas;
     let crc2;
@@ -8,34 +8,92 @@ var GenerativeKunst;
         canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
         crc2.clearRect(0, 0, canvas.width, canvas.height);
-        //lines
+        // background lines
         let colorLines = ["aqua", "teal", "aquamarine"];
         for (let i = 0; i < colorLines.length; i++)
-            for (let index = 0; index < 200; index++) {
-                let x = Math.floor(Math.random() * Math.floor(canvas.width));
-                let y = Math.floor(Math.random() * Math.floor(canvas.height));
-                let z = Math.floor(Math.random() * Math.floor(canvas.width));
-                let a = Math.floor(Math.random() * Math.floor(canvas.height));
+            for (let index = 0; index < 400; index++) {
+                let a = randomNumber(0, canvas.width);
+                let b = randomNumber(0, canvas.height);
+                let c = randomNumber(0, canvas.width);
+                let d = randomNumber(0, canvas.height);
                 crc2.beginPath();
                 crc2.strokeStyle = colorLines[i];
-                crc2.moveTo(x, y);
-                crc2.lineTo(z, a);
+                crc2.moveTo(a, b);
+                crc2.lineTo(c, d);
                 crc2.closePath();
                 crc2.stroke();
             }
-        // circles
-        let colorCircles = ["orange", "darkgoldenrod", "gold"];
-        for (let i = 0; i < colorCircles.length; i++)
-            for (let index = 0; index < 40; index++) {
-                crc2.beginPath();
-                crc2.arc(Math.floor(Math.random() * (canvas.width) + 1), Math.floor(Math.random() * (canvas.height) + 1), Math.floor(Math.random() * (50) + 1), 0, 3 * Math.PI);
-                crc2.stroke();
-                crc2.closePath();
-                crc2.fillStyle = colorCircles[i];
-                crc2.fill();
-            }
+        // foreground objects
+        let objectAmount = randomNumber(30, 300);
+        for (let i = 0; i < objectAmount; i++) {
+            drawRandomObject();
+        }
+        // reload button
         let newCanvas = document.querySelector("button");
         newCanvas.addEventListener("click", fillCanvas);
     }
-})(GenerativeKunst || (GenerativeKunst = {}));
+    function drawRandomObject() {
+        // random location within canvas
+        let x = randomNumber(0, canvas.width);
+        let y = randomNumber(0, canvas.height);
+        crc2.save();
+        crc2.translate(x, y);
+        crc2.fillStyle = randomColor();
+        // decide which shape
+        let objectShape = randomNumber(0, 3);
+        switch (objectShape) {
+            case 0:
+                randomRectangle();
+                break;
+            case 1:
+                randomCircle();
+                break;
+            case 2:
+                randomPolygon();
+                break;
+        }
+        crc2.restore();
+    }
+    // draw rectangle
+    function randomRectangle() {
+        let width = randomNumber(10, 100);
+        let height = randomNumber(10, 100);
+        crc2.fillRect(0, 0, width, height);
+    }
+    // draw circle
+    function randomCircle() {
+        let size = randomNumber(1, 100);
+        crc2.beginPath();
+        crc2.arc(0, 0, size, 0, 360);
+        crc2.closePath();
+        crc2.fill();
+    }
+    // draw polygon
+    function randomPolygon() {
+        let points = randomNumber(3, 8);
+        crc2.beginPath();
+        for (let i = 0; i < points; i++) {
+            let x = randomNumber(0, 100) - randomNumber(0, 100);
+            let y = randomNumber(0, 100) - randomNumber(0, 100);
+            crc2.lineTo(x, y);
+        }
+        crc2.closePath();
+        crc2.fill();
+    }
+    // random number between a minimum and a maximum input
+    function randomNumber(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    // random hex code for color
+    function randomColor() {
+        let letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 8; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+})(L04_GenerativeKunst || (L04_GenerativeKunst = {}));
 //# sourceMappingURL=generative_kunst.js.map
--- L04_Canvas/Sarah/generative_kunst.ts
+++ L04_Canvas/Sarah/generative_kunst.ts
@@ -1,4 +1,4 @@
-namespace GenerativeKunst {
+namespace L04_GenerativeKunst {
 
     window.addEventListener("load", fillCanvas);
 
@@ -12,40 +12,108 @@ namespace GenerativeKunst {
 
         crc2.clearRect(0, 0, canvas.width, canvas.height);
 
-        //lines
+        // background lines
         let colorLines: string[] = ["aqua", "teal", "aquamarine"];
 
         for (let i: number = 0; i < colorLines.length; i++)
-            for (let index: number = 0; index < 200; index++) {
-                let x: number = Math.floor(Math.random() * Math.floor(canvas.width));
-                let y: number = Math.floor(Math.random() * Math.floor(canvas.height));
-                let z: number = Math.floor(Math.random() * Math.floor(canvas.width));
-                let a: number = Math.floor(Math.random() * Math.floor(canvas.height));
-
+            for (let index: number = 0; index < 400; index++) {
+                let a: number = randomNumber(0, canvas.width);
+                let b: number = randomNumber(0, canvas.height);
+                let c: number = randomNumber(0, canvas.width);
+                let d: number = randomNumber(0, canvas.height);
+                
                 crc2.beginPath();
                 crc2.strokeStyle = colorLines[i];
-                crc2.moveTo(x, y);
-                crc2.lineTo(z, a);
+                crc2.moveTo(a, b);
+                crc2.lineTo(c, d);
                 crc2.closePath();
                 crc2.stroke();
             }
 
-        // circles
+        // foreground objects
+        let objectAmount: number = randomNumber(30, 300);
+        for (let i: number = 0; i < objectAmount; i++) {
+            drawRandomObject();
+        }
 
-        let colorCircles: string[] = ["orange", "darkgoldenrod", "gold"];
-
-        for (let i: number = 0; i < colorCircles.length; i++)
-            for (let index: number = 0; index < 40; index++) {
-        
-                crc2.beginPath();
-                crc2.arc(Math.floor(Math.random() * (canvas.width) + 1), Math.floor(Math.random() * (canvas.height) + 1), Math.floor(Math.random() * (50) + 1), 0, 3 * Math.PI);
-                crc2.stroke();
-                crc2.closePath();
-                crc2.fillStyle = colorCircles[i];
-                crc2.fill();
-            }
-
+        // reload button
         let newCanvas: HTMLElement = <HTMLElement>document.querySelector("button")!;
         newCanvas.addEventListener("click", fillCanvas);
+    }
+
+    function drawRandomObject(): void {
+        // random location within canvas
+        let x: number = randomNumber(0, canvas.width);
+        let y: number = randomNumber(0, canvas.height);
+
+        crc2.save();
+        crc2.translate(x, y);
+
+        crc2.fillStyle = randomColor();
+
+        // decide which shape
+        let objectShape: number = randomNumber(0, 3);
+        switch (objectShape) {
+            case 0:
+                randomRectangle();
+                break;
+            case 1:
+                randomCircle();
+                break;
+            case 2:
+                randomPolygon();
+                break;
+        }
+
+        crc2.restore();
+    }
+
+    // draw rectangle
+    function randomRectangle(): void {
+        let width: number = randomNumber(10, 100);
+        let height: number = randomNumber(10, 100);
+
+        crc2.fillRect(0, 0, width, height);
+    }
+
+    // draw circle
+    function randomCircle(): void {
+        let size: number = randomNumber(1, 100);
+
+        crc2.beginPath();
+        crc2.arc(0, 0, size, 0, 360);
+        crc2.closePath();
+        crc2.fill();
+    }
+
+    // draw polygon
+    function randomPolygon(): void {
+        let points: number = randomNumber(3, 8);
+
+        crc2.beginPath();
+        for (let i: number = 0; i < points; i++) {
+            let x: number = randomNumber(0, 100) - randomNumber(0, 100);
+            let y: number = randomNumber(0, 100) - randomNumber(0, 100);
+            crc2.lineTo(x, y);
+        }
+        crc2.closePath();
+        crc2.fill();
+    }
+
+    // random number between a minimum and a maximum input
+    function randomNumber(min: number, max: number): number {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    // random hex code for color
+    function randomColor(): string {
+        let letters: string = "0123456789ABCDEF";
+        let color: string = "#";
+        for (let i: number = 0; i < 8; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     }
 }
